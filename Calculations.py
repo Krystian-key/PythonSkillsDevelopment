04f4e932ac2236c507b12a655ae8052fbf574511
krystian-key <kluczkiewicz1512@wp.pl>
2022-11-27 19:21:26 +0100

    Finished another champter

---
+++ Calculations.py
@@ -0,0 +1,32 @@
+saldo = 40
+saldo += 30
+saldo -= 10
+
+print(saldo)
+
+lokata = 1000
+czynnik_akumulacji = 1 + 0.04
+lokata_po_roku = lokata * czynnik_akumulacji
+
+print('Wartość lokaty po roku: ', lokata_po_roku)
+
+pixel = 130
+pixel = pixel/255
+print(pixel)
+
+base = 2
+base **= 5
+print(base)
+
+x = 103
+x %=10
+print(x)
+
+imie = 'Krystian'
+nazwisko = 'Surname'
+nazwa = imie + ' ' + nazwisko
+print(nazwa)
+
+
+
+
