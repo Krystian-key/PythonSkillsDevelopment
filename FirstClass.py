114d41b48e3c31ef501e14f5bf2776ee18e0fe08
krystian-key <kluczkiewicz1512@wp.pl>
2022-11-27 17:25:12 +0100

    Learning Continues

--- FirstClass.py
+++ FirstClass.py
@@ -9,9 +9,30 @@ print(g + '\ntest')
 
 print('I love pyhton')
 
+print("test commit and push")
+print("\ttest commit and push")
+print("C:\path\\to\something\\new")
+print(r"C:\path\to\something\new")
 
+import os
 
+a = os.getcwd()
+print(a)
 
+print(""" Instrukcja uruchamiania pliku przyklad.py
+    -- file [nazwa pliku] 
+        zapisuje output do pliku
+    -- quiet 
+        wycisza logi w konsoli
+Koniec. """)
+
+string = "aktualności. "
+print(string * 3)
+
+age = 27
+name = 'kasia'
+print(str(age) + name)
+print('{} ma {} lat'.format(name, age))
 
 
 
