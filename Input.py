8877558644dcbe138f44612b959ddae8c4dd316b
krystian-key <kluczkiewicz1512@wp.pl>
2022-11-27 20:34:07 +0100

    Finished another champter

---
+++ Input.py
@@ -0,0 +1,12 @@
+# imie = input('Podaj Swoje imie: ')
+
+# print('Cześć {}'.format(imie))
+
+# jezyk = input('Podaj jezyk ktorego chcesz sie nauczyc: ')
+# print('A zatem nauczysz sie: {}'.format(jezyk))
+
+# name = input('Podaj imie: ')
+# age = input('Podaj wiek: ')
+# print('Czesc {}! Masz {} lat.'.format(name,age))
+
+age = int(input('Podaj wiek: '))
---
+++ Konwencja.py
@@ -0,0 +1,24 @@
+imie = 'Pavel'
+_imie = 'Damian'
+
+#camelCase
+malaDuza = 'Python 3.7'
+
+#PascalCase
+DuzaBezSpacji = 'Paschal'
+
+#snake_case
+snake_kase = 'z pauzami'
+
+#kebab-case
+kebabcase = 'kebab'
+
+#UPPER
+upper = 'upper'
+
+import keyword
+string = keyword.kwlist
+
+name = 'Python'
+print(name)
+print(name[0])
